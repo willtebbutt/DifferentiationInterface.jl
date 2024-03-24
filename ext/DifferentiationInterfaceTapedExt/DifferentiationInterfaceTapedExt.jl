620519c5504951ee20a5563301d19ff59d70fadb
Will Tebbutt <wct23@cam.ac.uk>
2024-03-24 12:19:02 +0000

    Update Taped interface

--- ext/DifferentiationInterfaceTapedExt/DifferentiationInterfaceTapedExt.jl
+++ ext/DifferentiationInterfaceTapedExt/DifferentiationInterfaceTapedExt.jl
@@ -12,7 +12,7 @@ function DI.value_and_pullback(f::F, ::AutoTaped, x, dy, extras::Nothing) where 
     # TODO: fix for https://github.com/withbayes/Taped.jl/issues/97
     y = f(x)
     # TODO: 
-    dy_righttype = convert(typeof(y), dy)
+    dy_righttype = convert(Taped.tangent_type(Taped._typeof(y)), dy)
     _, (_, dx) = value_and_pullback!!(rrule, dy_righttype, f, x)
     return y, dx
 end
